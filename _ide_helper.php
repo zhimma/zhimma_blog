431c194071a67d9aa7accc4dfa5495905fd413a5
mma <18710839146@163.com>
2018-02-27 23:35:08 +0800

    修改IDE helper

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.35 on 2018-02-27 23:31:45.
+ * Generated for Laravel 5.5.35 on 2018-02-27 23:34:55.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
